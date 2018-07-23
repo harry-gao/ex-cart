8c92ad4df4979307e05a98c17a34e577b187859d
Harry Gao <harry.gao@rea-group.com>
2018-07-23 21:33:13 +1000

    change title and  logo

--- priv/cart-client/src/components/Header/StoreHeader.js
+++ priv/cart-client/src/components/Header/StoreHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Header.css'
 
-import logo from '../../assets/icons/logo2.png'
+import logo from '../../assets/icons/logo.png'
 
 const StoreHeader = () =>
   (<div className={styles.storeHeader}>
     <div className={styles.logo} style={{backgroundImage: 'url(' + logo + ')'}} />
-    <div className={styles.title}> 植蔬宝新农人商城 </div>
+    <div className={styles.title}> 澳新商城 </div>
   </div>)
 
 export default StoreHeader
